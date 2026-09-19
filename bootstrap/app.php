97900e28eda9fd0bfbaa3b975c82af7af5b114ff
piouskenny <kehindeu13@gmail.com>
2026-09-19 22:11:00 +0100

    Fix Laravel trusted proxy configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
+
 <?php
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +14,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        // Trust Render's reverse proxy
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+        );
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
